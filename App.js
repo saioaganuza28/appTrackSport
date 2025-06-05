c898423862e93fdb7e88f092ca5fbc3c9f7f6245
saioaganuza28 <sganuzaado1@gmail.com>
2025-06-05 19:01:27 +0200

    cambios

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import InicioActividad from './components/pantallas/InicioActividadComponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <InicioActividad/>
       <StatusBar style="auto" />
     </View>
   );
